bd785a367e76fe9aa85561893130cf419f71bd86
renegf <renegferrari@gmail.com>
2020-01-31 21:53:08 +0000

    first push

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using System;
+
+public class AudioManager : MonoBehaviour
+{
+    //To use the audio manager simply use the next line where you want it
+    //FindObjectOfType<AudioManager>().Play("<Sound Name>");
+
+    public Sound[] sounds;
+
+    public static AudioManager instance;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        //Check if there is already a AudioManager, if yes destroy itself
+        if (instance == null)
+            instance = this;
+        else
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        DontDestroyOnLoad(gameObject);
+
+        foreach(Sound s in sounds)
+        {
+            s.source = gameObject.AddComponent<AudioSource>();
+            s.source.clip = s.clip;
+
+            s.source.volume = s.volume;
+            s.source.pitch = s.pitch;
+            s.source.loop = s.loop;
+        }
+    }
+
+    // Update is called once per frame
+    public void Play(string name)
+    {
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        if (s == null) 
+        {
+            Debug.LogWarning("Sound: " + name + " not found!");
+            return; 
+        }
+        s.source.Play();
+    }
+}
