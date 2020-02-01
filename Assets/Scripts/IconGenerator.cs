10ff2b513c9aa33f266f66441a725ce9656d0584
renegf <renegferrari@gmail.com>
2020-02-01 18:03:23 +0000

    Procedural Generation done

--- Assets/Scripts/IconGenerator.cs
+++ Assets/Scripts/IconGenerator.cs
@@ -1,15 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class IconGenerator : MonoBehaviour
 {
     public List<GameObject> tokens;
     private List<GameObject> quadrants = new List<GameObject>();
+    private List<Player> player = new List<Player>();
 
     // Start is called before the first frame update
     void Start()
     {
+        for (int i = 0; i < 4; i++)
+        {
+            player.Add(new Player(i));            
+        }
+
         quadrants.Add(transform.Find("TL").gameObject);
         quadrants.Add(transform.Find("TC").gameObject);
         quadrants.Add(transform.Find("TR").gameObject);
@@ -26,15 +33,19 @@ public class IconGenerator : MonoBehaviour
     {
         if (Input.GetKeyDown("space"))
         {
-            GenerateIcon();
+            GenerateIcon(player[Random.Range(0,player.Count)]);
         }
     }
 
-    private void GenerateIcon()
+    private void GenerateIcon(Player player)
     {
-        int cornerImage = Random.Range(0, tokens.Count);
-        int sideImage = Random.Range(0, tokens.Count);
-        int middleImage = Random.Range(0, tokens.Count);
+        int cornerImage = player.icons[0];
+        int sideImage = player.icons[1];
+        int middleImage = player.icons[2];
+
+        byte red = player.rgb[0];
+        byte green = player.rgb[1];
+        byte blue = player.rgb[2];
 
         //Destroy children before instantiating new ones
         foreach (GameObject quadrant in quadrants)
@@ -50,15 +61,24 @@ public class IconGenerator : MonoBehaviour
         {
             GameObject token = tokens[Random.Range(0, tokens.Count)];
 
+            //Set same corners
             if (quadrant.name == "TL" || quadrant.name == "TR" || quadrant.name == "BL" || quadrant.name == "BR")
                 token = tokens[cornerImage];
+            //Set same sides
             else if (quadrant.name == "ML" || quadrant.name == "MR" || quadrant.name == "TC" || quadrant.name == "BC")
                 token = tokens[sideImage];
+            //Set Center
             else if (quadrant.name == "MC")
                 token = tokens[middleImage];
 
-            var createImage = Instantiate(token) as GameObject;
+            GameObject createImage = Instantiate(token) as GameObject;
             createImage.transform.SetParent(quadrant.transform, false);
+
+         
+            foreach (Transform child in quadrant.transform)
+            {
+                child.GetComponent<Image>().color = new Color32(red, green, blue, 255);
+            }
         }
     }
 }
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+
+public class Player
+{
+    private int playerID;
+
+    public List<int> icons = new List<int>();
+
+    public List<byte> rgb = new List<byte>();
+
+    public Player(int identification)
+    {
+        playerID = identification;
+
+        Random rand = new Random(playerID);
+
+        icons.Add(rand.Next(0, 4));
+        icons.Add(rand.Next(0, 4));
+        icons.Add(rand.Next(0, 4));
+
+        rgb.Add((byte)rand.Next(0, 255));
+        rgb.Add((byte)rand.Next(0, 255));
+        rgb.Add((byte)rand.Next(0, 255));
+        UnityEngine.Debug.Log("Player: " + playerID + "created");
+    }
+}
