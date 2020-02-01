b4a772374c6a48d1845b0ef4986499008f086a1e
Chi <felixhu0312@gmail.com>
2020-02-01 13:02:56 +0000

    StoryController added, ButtonSpawner modified

--- Assets/Scripts/ButtonSpawner.cs
+++ Assets/Scripts/ButtonSpawner.cs
@@ -1,44 +1,71 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.UI;
-
+using DG.Tweening;
 
 public class ButtonSpawner : MonoBehaviour{
 
-    public Image imageHolder;
+    public static ButtonSpawner _instance;
+
+    public Image imageHolder1, imageHolder2;
+    int imageHolderFlag = 1;
+    public Vector3 topImagePosition, middleImagePosition, bottomImagePosition;
+
     public float shakeMagnitude = 3f;
     public Sprite[] icons;
 
+    private void Awake() {
+        _instance = _instance ? _instance : this;
+        //DontDestroyOnLoad(_instance);
+    }
+
+    private void Start() {
+        topImagePosition = imageHolder2.transform.position;
+        middleImagePosition = imageHolder1.transform.position;
+        bottomImagePosition = imageHolder1.transform.position - (imageHolder2.transform.position - imageHolder1.transform.position);
+        imageHolder1.sprite = icons[Random.Range(0, icons.Length)]; // Init
+    }
+
     public void ButtonPressed(bool correctButtonPressed){
         if(correctButtonPressed){
-            imageHolder.sprite = icons[Random.Range(0, icons.Length)];
-            StartCoroutine(ImageHolderShake());
+
+            if(imageHolderFlag == 1){
+                imageHolder1.sprite = icons[Random.Range(0, icons.Length)];
+            }else if(imageHolderFlag == 2){
+                imageHolder2.sprite = icons[Random.Range(0, icons.Length)];
+            }
+
+            ChangeImage();
         }
     }
 
     private void Update() {
         if(Input.anyKeyDown){
-            ButtonPressed(true);
+            StoryController._instance.SymbolWasMatched();
         }
     }
 
-    IEnumerator ImageHolderShake(){
+    public void ChangeImage(){
 
-        Vector3 originalPosition = transform.position;
+        // Switch positions between two image holders
+        // DOMove is from DOTween, a tool to create lerp animations without calling Lerp or Coroutines
+        if (imageHolderFlag == 1) {
+            imageHolder1.transform.position = middleImagePosition;
+            imageHolder1.transform.DOMove(bottomImagePosition, 0.15f);
 
-        float timeElapsed = 0.3f;
-        float duration = 0.3f;
+            imageHolder2.transform.position = topImagePosition;
+            imageHolder2.transform.DOMove(middleImagePosition, 0.15f);
 
-        while(timeElapsed > 0f){
+        } else if (imageHolderFlag == 2) {
+            imageHolder1.transform.position = topImagePosition;
+            imageHolder1.transform.DOMove(middleImagePosition, 0.15f);
 
-            transform.position = originalPosition + Vector3.Normalize(new Vector3(Random.Range(-1,1), Random.Range(-1, 1), Random.Range(-1, 1))) * (timeElapsed/duration) * shakeMagnitude;
-
-            timeElapsed -= Time.deltaTime;
-            yield return null;
+            imageHolder2.transform.position = middleImagePosition;
+            imageHolder2.transform.DOMove(bottomImagePosition, 0.15f);
         }
 
-        transform.position = originalPosition;
+        // Switch Flag
+        imageHolderFlag = imageHolderFlag == 1 ? 2 : 1;
     }
+
 
 }
---
+++ Assets/Scripts/StoryController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+public class StoryController : MonoBehaviour
+{
+    public static StoryController _instance;
+
+    /*
+    public StoryController Instance()
+    {
+        if (_instance == null) _instance = this;
+        return _instance;
+    }
+    */
+
+    private void Awake() {
+        _instance = _instance ? _instance : this;
+        DontDestroyOnLoad(_instance);
+    }
+
+    public void SymbolWasMatched()
+    {
+        Debug.Log("Symbol was matched");
+        ButtonSpawner._instance.ButtonPressed(true);
+    }
+
+    public void SymbolWasMisMatched()
+    {
+        Debug.Log("Symbol was mismatched");
+    }
+
+    public void ReachedWinState()
+    {
+        Debug.Log("Reached win state");
+    }
+
+    public void ReachedLoseState()
+    {
+        Debug.Log("Reached lose state");
+    }
+}
